32967a4a6e983fa6a120c8158632b1a4bd4b4acb
sutthirakk28 <sutthirakk28@users.noreply.github.com>
2017-04-28 10:44:31 +0700

    1.1

---
+++ index.php
@@ -0,0 +1,383 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="วอลเปเปอร์ ติดผนัง">
+    <meta name="keywords" content="วอลเปเปอร์ ติดผนัง" />
+    <meta name="author" content="">
+
+    <title>P.K. Wallpaper home</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="css/demo.css" />
+    <link rel="stylesheet" type="text/css" href="css/style4.css" />
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+        
+    <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+    <style type="text/css">
+        section {
+            padding: 100px 0;
+            background-color: white;
+            
+        }        
+    </style>
+    
+<script language="JavaScript1.2">
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+//if IE4+
+document.onselectstart=new Function ("return false")
+//if NS6
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
+</script>
+<script language=JavaScript>
+<!--
+var message="Function Disabled!";
+///////////////////////////////////
+function clickIE() {if (document.all) {alert(message);return false;}}
+function clickNS(e) {if 
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {return false;}}}
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+document.oncontextmenu=new Function("return false")
+// --> 
+</script>
+<!--  #########                Popup    -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-87955519-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+        <style type="text/css">
+            #Mypop { display:none; }
+            .button {
+                background-color: #2b91af;
+                border-radius: 10px;
+                box-shadow: 0 2px 3px rgba(0,0,0,0.3);
+                color: #fff;
+                cursor: pointer;
+                display: inline-block;
+                padding: 10px 20px;
+                text-align: center;
+                text-decoration: none
+            }
+            .button.small {
+                border-radius: 15px;
+                float: right;
+                margin: 22px 5px 0;
+                padding: 6px 15px
+            }
+            .button:hover {
+                background-color: #1e1e1e
+            }
+            .button>span {
+                font-size: 84%
+            }
+            .button.b-close,.button.bClose {
+                border-radius: 7px 7px 7px 7px;
+                box-shadow: none;
+                font: bold 131% sans-serif;
+                padding: 0 6px 2px;
+                position: absolute;
+                right: -7px;
+                top: -7px
+            }
+            #popup{
+                background-color: #fff;
+                border-radius: 10px 10px 10px 10px;
+                color: #111;
+                display: none;
+                padding: 15px;
+                margin-top: 20px;
+            }
+            #popup1{
+                background-color: #fff;
+                border-radius: 10px 10px 10px 10px;
+                color: #111;
+                display: none;
+                padding: 15px;
+                margin-top: 20px;
+            }
+            .slide-image1{
+                width: 100%;
+                height: auto;
+            }
+            .a20{
+                font-family: myFirstFont;
+            }
+        </style>
+    <div id="popup">
+        <span class="button b-close"><span>X</span></span>
+        <img src="img/popup/pk.png" width="540" height="380" class="img-responsive">
+    </div>
+    <!--<div id="popup1">
+        <span class="button b-close"><span>X</span></span>
+        <img src="img/popup/des.png" width="1000" height="660" class="img-responsive">
+    </div>-->
+
+<script src="js/jquery.bpopup.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript">
+            ;
+            (function ($) {
+                // DOM Ready
+                $(function () {
+                    $('#popup').bPopup({
+                        easing: 'easeOutBack',
+                        speed: 450,
+                        transition: 'slideDown',
+                        transitionClose: 'slideUp',
+                        // modalClose: false,
+                        onClose: function () {
+                            /*$('#popup1').bPopup({
+                            easing: 'easeOutBack',
+                            speed: 450,
+                            transition: 'slideDown',
+                            transitionClose: 'slideUp',
+                            // modalClose: false,
+                            onClose: function () {
+
+                        }
+                    });*/
+                        }
+                    });
+                });
+            })(jQuery);
+    </script>
+    <!--  #########                Popup    -->
+    
+
+</head>
+
+<body id="page-top">
+    
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">P.K. Wallpaper home</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">เกี่ยวกับเรา</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">บริการ</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">สินค้า</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">ติอต่อ</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <ul class="cb-slideshow">
+            <li><span>Image 01</span>
+            <li><span>Image 02</span>
+            <li><span>Image 03</span>
+            <li><span>Image 04</span>
+            <li><span>Image 05</span>
+            <li><span>Image 06</span>
+        </ul>
+        <div class="header-content">
+            
+            <div class="header-content-inner">
+                <div class="img"><image src="img/pk.png" /></div>
+                <!--<h1 id="homeHeading">WALLPAPER</h1>
+                <hr>
+                <p>ยังมีผลงานอื่นๆ อีกมาก เช่น Brand Identity อย่างโลโก้ ที่มาพร้อมกับเซทของนามบัตร หัวจดหมาย หัวแฟกซ์ งานป้ายต่างๆ ขององค์กร ไม่จำกัดว่าจะเป็นการเริ่มต้นธุรกิจใหม่หรือ Rebranding นอกจากนั้นงานออกแบบสิ่งพิมพ์อย่าง แคตตาล็อค ใบปลิว แผ่นพับ โฆษณา คู่มือ</p>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>-->
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading a1">P.K. Wallpaper home</h2>
+                    <hr class="light">
+                    <p class="text-faded">อีกหนึ่งระดับของการตกแต่งบ้านให้ดูน่าอยู่ นั้นคือการติดตั้งวอลเปเปอร์ลายสวยๆ <br>
+                        ตามสไตล์ที่คุณสามารถเลือกเองได้ โดยที่คุณไม่ต้องยุ่งยากกับการเลือกซื้ออีกต่อไป <br>....พีเค วอลเปเปอร์ โฮม มีจำหน่ายครบครัน ไม่ว่าจะเป็น วอลเปเปอร์ 3 มิติ วอลเปเปอร์สติ๊กเกอร์ในตัว และอื่นๆ อีกมากมาย</p>
+                    <a href="#services" class="page-scroll btn btn-default btn-xl sr-button">บริการของเรา</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">บริการของเรา</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-diamond text-primary sr-icons"></i>
+                        <h3 class="p3">คุณภาพดี</h3>
+                        <p class="text-muted p2">นำเข้าสินค้าจากต่างประเทศ</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
+                        <h3 class="p3">บริการรวดเร็วทันใจ</h3>
+                        <p class="text-muted p2">ส่งของตามระยะเวลาที่ลูกค้ากำหนด</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
+                        <h3 class="p3">มีผลงานจริง</h3>
+                        <p class="text-muted p2">สินค้ารับรองคุณภาพ และความพึงพอใจ</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
+                        <h3 class="p3">บริการด้วยใจ</h3>
+                        <p class="text-muted p2">ใส่ใจในสินค้า และการบริการ</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter popup-gallery">
+                <?php 
+                for($i=1;$i<=169;$i++){
+                    echo"<div class='col-lg-4 col-sm-6'>
+                    <a href='img/portfolio/fullsize/pk-$i.png' class='portfolio-box a1'>
+                        <img src='img/portfolio/thumbnails/pk-$i.png' class='img-responsive' alt=''>
+                        <div class='portfolio-box-caption'>
+                            <div class='portfolio-box-caption-content'>
+                                <div class='project-category text-faded'>
+                                    P.K. Wallpaper home
+                                </div>                                
+                            </div>
+                        </div>
+                    </a>
+                </div>";
+                }
+                
+                ?>
+            </div>
+        </div>
+    </section>
+
+    
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading"><i class="f1 fa fa-cog fa-spin fa-2x fa-fw"></i>ติดต่อเรา<i class="f1 fa fa-cog fa-spin fa-2x fa-fw"></i></h2>
+                    <hr class="primary">                    
+                </div>
+                <div class="col-lg-3 text-center">
+                    <i class="fa fa-mobile" aria-hidden="true"></i>
+                    <p class="p1">092-521-1899</p>
+                </div>
+                <div class="col-lg-6 text-center">
+                    <i class="fa fa-line-chart fa-3x" aria-hidden="true"></i>
+                    <p class="p1"><a href="http://line.me/ti/p/GE01RHD6dR" target="_blank">Line ID : kumong23</a> /
+                        <a href="https://line.me/R/ti/p/%40jza4105a" target="_blank" class="a20">ติดตามข่าวสาร และโปรโมชั่น</a>
+                    </p>
+                </div>
+                <div class="col-lg-3 text-center">
+                    <i class="fa fa-facebook-square fa-4x" aria-hidden="true"></i>
+                    <p class="p1"><a href="https://www.facebook.com/Wallpaper-Home-1118602184859692/?ref=bookmarks" target="_blank">Wallpaper Home</a></p>
+                </div>
+               
+            </div>            
+        </div>
+    </section>
+    
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/58412b9373e3d85bf11f5eb2/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+    
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
